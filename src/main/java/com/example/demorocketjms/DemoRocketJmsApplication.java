b33db6cccc4674573110b8eee266c0b7fb8e70a4
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2020-03-03 08:01:32 +0530

    first commit

---
+++ src/main/java/com/example/demorocketjms/DemoRocketJmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorocketjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRocketJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRocketJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demorocketjms/DemoRocketJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorocketjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRocketJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
