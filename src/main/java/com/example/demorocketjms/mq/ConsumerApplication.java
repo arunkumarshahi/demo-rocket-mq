2f4d3af9a6b671ddfeff39c3cdf4f24e1ca401c5
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2020-03-03 09:03:57 +0530

    added code for rocket mq producer and consumer

---
+++ src/main/java/com/example/demorocketjms/mq/ConsumerApplication.java
@@ -0,0 +1,40 @@
+package com.example.demorocketjms.mq;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import static org.apache.rocketmq.spring.annotation.SelectorType.SQL92;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
+
+
+
+@Log4j2
+@Service
+@RocketMQMessageListener(
+        topic = "greetings-topic",
+        consumerGroup = "simple-group"
+)
+class SimpleConsumer implements RocketMQListener<Greeting> {
+
+    @Override
+    public void onMessage(Greeting greeting) {
+        log.info(greeting.toString());
+    }
+}
+
---
+++ src/main/java/com/example/demorocketjms/mq/ProducerApplication.java
@@ -0,0 +1,50 @@
+package com.example.demorocketjms.mq;
+
+import lombok.*;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.core.MessagePostProcessor;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.time.Instant;
+
+@RequiredArgsConstructor
+@SpringBootApplication
+public class ProducerApplication {
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> ready(RocketMQTemplate template) {
+        return event -> {
+
+            var now = Instant.now();
+            var destination = "greetings-topic";
+
+            for (var name : "Tammie,Kimly,Josh,Rob,Mario,Mia".split(",")) {
+
+                var payload = new Greeting("Hello @ " + name + " @ " + now.toString());
+                var messagePostProcessor = new MessagePostProcessor() {
+
+                    @Override
+                    public Message<?> postProcessMessage(Message<?> message) {
+                        var headerValue = Character.toString(name.toLowerCase().charAt(0));
+                        return MessageBuilder
+                                .fromMessage(message)
+                                .setHeader("letter", headerValue)
+                                .build();
+                    }
+                };
+                template.convertAndSend(destination, payload, messagePostProcessor);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+}
+
